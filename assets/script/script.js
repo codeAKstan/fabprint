16caecdc0adcc2cda14d9edefa9cf880a1b5b251
codeAKstan <boystanismoses@gmail.com>
2025-02-22 18:34:53 +0100

    addded index page, header

---
+++ assets/script/script.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var mobileNav = document.getElementById('mobile-nav');
+    mobileNav.classList.toggle('active');
+});
