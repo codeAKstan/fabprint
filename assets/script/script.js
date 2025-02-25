dd4fcd05970c0d605a6ec5b419bbab4ee85576c7
codeAKstan <boystanismoses@gmail.com>
2025-02-25 12:19:19 +0100

    Added image slider

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,19 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
     var mobileNav = document.getElementById('mobile-nav');
     mobileNav.classList.toggle('active');
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+    const carouselInner = document.querySelector('.carousel-inner');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const totalItems = carouselItems.length;
+
+    function moveToNextSlide() {
+        currentIndex = (currentIndex + 1) % totalItems;
+        const offset = -currentIndex * 100;
+        carouselInner.style.transform = `translateX(${offset}%)`;
+    }
+
+    setInterval(moveToNextSlide, 5000);
+});
